7f183020a8a33f70dd55ccd2be695f562bfd8444
Jiri Vitek <citanus@gmail.com>
2014-10-03 03:04:42 +0200

    added basic user list view with online/offline status

--- views/site/messenger.php
+++ views/site/messenger.php
@@ -3,22 +3,33 @@
 use yii\helpers\Html;
 use yii\web\View;
 use yii\helpers\Url;
+use yii\grid\GridView;
+use yii\data\ActiveDataProvider;
+use app\models\User;
+
 /* @var $this yii\web\View */
 $this->title = 'My Yii Application';
 
+$userDataProvider = new ActiveDataProvider([
+	'query' => User::find()->where(sprintf('id != %d', Yii::$app->user->identity->getId())),
+	'pagination' => [
+		'pageSize' => 20,
+	],
+]);
 
-// register simple javascript which will periodicaly update online status for current user
-$script = '
-function updateMessenger() {
-	//console.log("updating status - start ");
+
+/** register simple javascript which will periodicaly:
+ *  - update online status for current user
+ *  - update user list
+ *  - update user informatins
+ **/
+$this->registerJs('function updateMessenger() {
+	$.pjax.reload({container:"#usersList"});
 	$.get("'.Url::to(['update-online-status']).'");
-	//	console.log("updating status - end");
 }
-
-//console.log("update messenger interval set");
 setInterval(updateMessenger,5000);
-';
-$this->registerJs($script, View::POS_READY);
+', View::POS_READY);
+
 ?>
 <div class="site-index">
 
@@ -27,7 +38,22 @@ $this->registerJs($script, View::POS_READY);
             <div class="col-lg-2">
                 <h2>User list</h2>
 
-
+	            <?php \yii\widgets\Pjax::begin(); ?>
+	            <?= GridView::widget([
+		            'dataProvider' => $userDataProvider,
+		            'id' => 'usersList',
+		            'columns' => [
+			            'name',
+			            'status',
+			            [
+				            'class' => 'yii\grid\DataColumn', // can be omitted, default
+			                'value' => function ($data) {
+					            return $data->isOnline()?'online':'offline';
+				            },
+				        ],
+		            ],
+	            ]);?>
+	            <?php \yii\widgets\Pjax::end(); ?>
 
 
             </div>
